050f5ff369e3ad1d96f4e30368ab765de7ed0be2
Corbin Woods <112423356+Aleph1237@users.noreply.github.com>
2024-03-26 13:54:07 -0600

    Delete Website directory

--- Website/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const welcomeText = document.getElementById('welcomeText');
-    const changeColorBtn = document.getElementById('changeColorBtn');
-
-    changeColorBtn.addEventListener('click', function() {
-        welcomeText.style.color = welcomeText.style.color === 'red' ? '#333' : 'red';
-    });
-});
